0a29a5d666944e3e053d3129852c0b4f6acabd8d
ZacLinz <zaclinzmeyer@gmail.com>
2019-08-10 14:56:17 -0600

    add events

--- src/Event.js
+++ src/Event.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+
 
 class Event extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      modalIsOpen: false
+    };
+  }
 
   state = {
 
   };
 
+  showDetails = (event) => {
+
+  }
+
+
   render() {
     return (
       <div>
+        <button onClick={() => this.showDetails()}
       </div>
     );
   }
-}
+};
 
 export default Event;
