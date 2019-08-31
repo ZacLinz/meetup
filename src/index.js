39b78cd39802619da7687ce6e6bdb42c0b4356a8
ZacLinz <zaclinzmeyer@gmail.com>
2019-08-31 16:50:20 -0600

    add atatus

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as atatus from 'atatus-js';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-atatus.config('5f6af30a6bfb4e9eb6251b5a493a984c').install();
+atatus.config('9a600865f49d4e8b9f5dabd6c6cfebd4').install();
 atatus.notify(new Error('Test Atatus Setup'));
